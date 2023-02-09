801e28e82ddaee621fcb3481910afe92f4e75a14
Henrik Sundman <henriksundman@yahoo.se>
2023-02-09 16:38:40 +0100

    remove imports

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,12 +2,9 @@ import React, { useState } from 'react';
 
 import GuessInput from '../GuessInput';
 import GuessResults from '../GuessResults';
-import Guess from '../Guess/Guess';
 
 import { sample } from '../../utils';
-import { range } from '../../utils';
 import { WORDS } from '../../data';
-import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
